867b9ecb63381a4cc78ae31062bb39411b9175ab
spettigrew <sara.pettigrew5@gmail.com>
2019-12-19 19:47:23 -0700

    Notes added

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3", // our DBMS driver
+    useNullAsDefault: true, //necessary when using SQLite
+    connection: {
+      filename: "./produce.db3",
+    },
+  },
+};
