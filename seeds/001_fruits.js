a6fce5541149ced5760f540b372d4d0a7c1dc3cf
spettigrew <sara.pettigrew5@gmail.com>
2019-12-19 19:49:30 -0700

    Seed setup

---
+++ seeds/001_fruits.js
@@ -0,0 +1,14 @@
+
+exports.seed = async function(knex) {
+  await knex("fruits").truncate()
+  await knex("fruits").insert([
+    { name: "dragonfruit", avgWeightOz: 16.7, delicious: true, color: "red" },
+    { name: "strawberry", avgWeightOz: 0.42, delicious: true, color: "red" },
+    { name: "banana", avgWeightOz: 4.0, delicious: true, color: "yellow" },
+    { name: "noni", avgWeightOz: 1.0, delicious: false, color: "white" },
+  ])
+}
+
+
+// Seed data, or test/sample data, are really useful for testing.
+// We can run the seeds to reset all the data in a DB back to it's initial state.
